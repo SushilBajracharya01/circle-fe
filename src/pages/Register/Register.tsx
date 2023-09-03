d57b9b4d5491baf785e52d1afce3b55260c7570a
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-03 17:05:25 +0545

    feat: register page created

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,135 @@
+import Button from "../../components/Button"
+import Input from "../../components/Input"
+import Logo from "../../components/Logo"
+
+import RegisterImage from '../../assets/register-img.svg';
+import { Link } from "react-router-dom";
+
+function RegisterPage() {
+    return (
+        <div className="container mx-auto min-h-screen py-8 px-4">
+            <div className="md:mb-5">
+                <Logo />
+            </div>
+            <form className="md:space-y-8 divide-y">
+                <div className="grid md:grid-cols-2 ">
+                    <div className="hidden md:block">
+                        <div>
+                            <img src={RegisterImage} className="w-full object-contain max-w-[600px]" />
+                            <span className="text-gray-300">
+                                <a className="" href="https://www.freepik.com/free-vector/crowd-diverse-people-standing-together-around-empty-banner_8609070.htm#query=community%20vector&position=3&from_view=search&track=ais">Image by pch.vector</a> on Freepik
+                            </span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+                            <h2 className="text-4xl font-bold text-gray-800">Register your Account</h2>
+                            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                                Fill up this short form to join us and create your circle.
+                            </p>
+
+                            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                <label htmlFor="username" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                    Username
+                                </label>
+                                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div className="max-w-lg">
+                                        <Input
+                                            name="username"
+                                            isRequired
+                                            type="text"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                <label htmlFor="fullname" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                    Fullname
+                                </label>
+                                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div className="max-w-lg">
+                                        <Input
+                                            name="fullname"
+                                            isRequired
+                                            type="text"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                    Email
+                                </label>
+                                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div className="max-w-lg">
+                                        <Input
+                                            name="email"
+                                            isRequired
+                                            type="email"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                                <label htmlFor="password" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                                    Password
+                                </label>
+                                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div className="max-w-lg">
+                                        <Input
+                                            name="password"
+                                            isRequired
+                                            type="password"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center sm:border-t sm:border-gray-200 sm:pt-5">
+                                <label htmlFor="photo" className="block text-sm font-medium text-gray-700">
+                                    Photo
+                                </label>
+                                <div className="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div className="flex items-center">
+                                        <span className="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                                            <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                                            </svg>
+                                        </span>
+                                        <button
+                                            type="button"
+                                            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        >
+                                            Change
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mt-10">
+                            <Button
+                                label="Submit"
+                                onClick={() => { }}
+                            />
+                        </div>
+
+                        <div className="mt-4 text-gray-700">
+                            Already have an account? <Link to="/" className="ml-2 font-bold text-blue-600">Sign in</Link>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterPage;
