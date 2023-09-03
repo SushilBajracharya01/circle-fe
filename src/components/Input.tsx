7d411583d385f1ecc51abb2fc2eb79b79ac190ce
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-03 17:05:13 +0545

    refactor: input color class updated to custom primary classes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -16,7 +16,7 @@ function Input({ label, name, type, isRequired }: IInputProps) {
                     name={name}
                     type={type}
                     required={isRequired}
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
                 />
             </div>
     }
