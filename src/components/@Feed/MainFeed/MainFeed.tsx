ebd7507afd96d15fa4cccafaf14f4e20077a15ba
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-04 22:40:40 +0545

    feat: feed ui update

---
+++ src/components/@Feed/MainFeed/MainFeed.tsx
@@ -0,0 +1,5 @@
+export default function MainFeed() {
+    return (
+        <div className="scroll-main main-feed w-full lg:w-[680px] mx-auto bg-purple-100">MainFeed</div>
+    )
+}
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,5 +1,13 @@
+import MainFeed from "../../components/@Feed/MainFeed/MainFeed";
+
 export default function Feed() {
   return (
-    <div>Feed</div>
+    <div className="grid grid-cols-12 md:gap-4">
+      <div className="hidden md:block col-span-3 lg:col-span-2 bg-green-50">Sidebar</div>
+      <div className="col-span-12 md:col-span-6 lg:col-span-8 bg-red-50">
+        <MainFeed />
+      </div>
+      <div className="hidden md:block col-span-3 lg:col-span-2 bg-blue-50">Sidebar</div>
+    </div>
   )
 }
