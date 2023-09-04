6f9ed59f8026f89909b861f1a9b9fc6438220b3a
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-04 17:32:40 +0545

    refactor: input style unified

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({ label, name, type, isRequired, register, errors 
                     ...(register ? register(name) : {})
                     }
                     // required={isRequired}
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    className="circle_input"
                 />
             </div>
     }
