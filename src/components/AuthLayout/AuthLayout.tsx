0c1d4c850d20e7228761d72102573392f6e9ddb0
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-07 09:53:54 +0545

    feat: /me api integration and store in redux

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,7 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { useQueryHook } from "../../hooks/react-query/useQueryHook";
+import { useEffect } from "react";
+import { handleSetUser } from "../../_redux/userSlice";
+import { useAppDispatch } from "../../_redux/redux";
 
 export default function AuthLayout() {
+    const dispatch = useAppDispatch();
+
+    const { data: userData } = useQueryHook({
+        queryName: 'me',
+        queryRoute: `/users/me`,
+        options: {
+            staleTime: 120000
+        }
+    });
+
+    useEffect(() => {
+        dispatch(handleSetUser(userData));
+    }, [dispatch, userData]);
+
+
     return (
         <div className="auth max-h-screen">
             <Header />
