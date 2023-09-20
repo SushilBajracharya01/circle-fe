a7514cfcb17f91fdb364ce5cf5cbbe7ccbbb46f7
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-20 23:56:02 +0545

    fix: image uploader accept only image

--- src/components/PhotoInput/PhotoInput.tsx
+++ src/components/PhotoInput/PhotoInput.tsx
@@ -12,6 +12,7 @@ export default function PhotoInput({ previewUrl, onPhotoChange, className, isPeo
                 onChange={onPhotoChange}
                 className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 disabled={disabled}
+                accept="image/*"
             />
         </div>
     )
