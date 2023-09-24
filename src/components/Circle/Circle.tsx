03850f1b8cb2b95d1f0515af8c6594f1587d26d1
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-24 22:01:29 +0545

    feat: minor ui update

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -4,7 +4,7 @@ import Avatar from "../Avatar";
 
 export default function Circle({ circle }: ICircleProps) {
     return (
-        <Link to={`circle/${circle._id}`} className="text-center cursor-pointer" key={circle._id}>
+        <Link to={`circle/${circle._id}`} className="text-center py-4 rounded-md transition-colors cursor-pointer hover:bg-white" key={circle._id}>
             <Avatar isCloudinary url={circle.photo?.public_id} size="xl" isPeople={false} />
             <h1 className="text-xl font-medium text-gray-900">{circle.name}</h1>
             {circle.moto && <p className="text-sm italic text-gray-800">"{circle.moto}"</p>}
