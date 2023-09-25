aa59ba2ed9c6542e0048b9864799fedb20977ab4
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-25 23:58:38 +0545

    feat: skeleton loading for sidebar item

--- src/components/@Feed/MainFeed/SidebarItem.tsx
+++ src/components/@Feed/MainFeed/SidebarItem.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import { ISidebarItemProps } from "../../../types";
+import Skeleton from "react-loading-skeleton";
 
 export default function SidebarItem({ icon, title, href }: ISidebarItemProps) {
     return (
         <Link to={href} className="flex p-2 bg-gray-100 hover:bg-gray-200 transition-colors duration-200 delay-75 rounded-md items-center gap-3">
-            {icon}
+            {icon ? icon : <Skeleton circle width={40} height={40} />}
 
-            {title && <div className="font-medium">{title}</div>}
+            {
+                <div className="font-medium w-full">
+                    {title ? title : <Skeleton count={0.7} />}
+                </div>
+            }
         </Link>
     )
 }
