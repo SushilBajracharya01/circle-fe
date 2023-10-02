8b29c1158eb3b1c3c02780d7db6e8c89cd649d31
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-10-02 18:12:31 +0545

    feat: profile ui update

--- src/components/@Feed/MainFeed/Sidebar.tsx
+++ src/components/@Feed/MainFeed/Sidebar.tsx
@@ -1,3 +1,4 @@
+import Skeleton from "react-loading-skeleton";
 import { ISidebarProps } from "../../../types";
 import Avatar from "../../Avatar";
 import SidebarItem from "./SidebarItem";
@@ -5,7 +6,24 @@ import SidebarItem from "./SidebarItem";
 export default function Sidebar({ user }: ISidebarProps) {
     return (
         <div>
-            <SidebarItem icon={<Avatar url={user?.photo?.public_id} isCloudinary size="sm" />} title={user?.fullname} href="/profile" />
+            <div className="rounded-md bg-slate-100 p-4 mb-3">
+                <div className="flex justify-center">
+                    <Avatar url={user?.photo?.public_id} isCloudinary size="lg" />
+                </div>
+
+                <div className="text-center text-sm text-gray-700 mt-2 break-words">
+                    <h3 className="text-lg font-bold">
+                        {
+                            user?.fullname ? user?.fullname : <Skeleton count={0.7} />
+                        }
+                    </h3>
+                    @{
+                        user?.username ? user?.username : <Skeleton count={0.} />
+                    }
+                </div>
+            </div>
+
+            <SidebarItem title={user?.fullname} href="/profile" />
         </div>
     )
 }
