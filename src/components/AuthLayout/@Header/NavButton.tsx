735f5c92280f7903d6683976f678e219b7f20bef
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-10-04 01:54:14 +0545

    feat: header ui update

--- src/components/AuthLayout/@Header/NavButton.tsx
+++ src/components/AuthLayout/@Header/NavButton.tsx
@@ -4,7 +4,7 @@ import { INavButtonProps } from "../../../types";
 export default function NavButton({ to, label, icon }: INavButtonProps) {
     return (
         <NavLink to={to} className={({ isActive }) => {
-            return `p-2 ${isActive ? 'border-4 border-primary-500 bg-primary-100 rounded-full' : ''}`
+            return `p-2 ${isActive ? 'border-4 border-primary-500 rounded-lg text-primary-500' : ''}`
         }}>
             <span className="sr-only">{label}</span>
             {icon}
