2d5a756d8577d6b7a80a67d6f61fd5b313012b92
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-10-04 20:24:15 +0545

    refactor: color update in horizaontal line

--- src/components/HorizontalLine.tsx
+++ src/components/HorizontalLine.tsx
@@ -6,7 +6,7 @@ import { IHorizontalLineProps } from "../types";
 export default function HorizontalLine({ className }: IHorizontalLineProps) {
     return (
         <div className={className ?? ""}>
-            <hr className="w-[99%] border-t-2 mx-auto my-3" />
+            <hr className="w-[99%] border-gray-300 mx-auto my-2" />
         </div>
     )
 }
